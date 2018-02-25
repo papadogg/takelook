8eb3970f6f3fa0e2bb7bd1a2f684fbec3a8d1290
papadog <papadoginc@gmail.com>
2018-02-25 10:30:32 +0300

    task1

--- src/components/StudiosList.js
+++ src/components/StudiosList.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import StudiosItem from './StudiosItem';
 
 const StudiosList = ({ studios }) => {
-  const studiosList = studios.map(studio => <StudiosItem key={studio.id} {...studio} />);
+  const studiosList = studios
+    .sort((a, b) => a.price - b.price)
+    .map(studio => <StudiosItem key={studio.id} {...studio} />);
   return <div>{studiosList}</div>;
 };
 
